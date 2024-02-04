d6a99140b004eca1d2b045d43882e462e041d258
akib012 <jafriakib12@gmail.com>
2024-02-04 16:48:58 +0530

    chngs

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Providers>{children}</Providers>
+      <body>{<Providers>{children}</Providers>}</body>
     </html>
   )
 }
